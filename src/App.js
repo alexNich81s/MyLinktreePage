c87cf3faf95a19a593e222692110fcacc3fd06ec
Ethan Nichols <eanichols97@gmail.com>
2022-12-03 23:13:08 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Ethan Nichols</h1>
+      <h2>Links:</h2>
+      <ul>
+        <li><button><a href="https://github.com/alexNich81s/">GitHub</a></button></li>
+        <li><button><a href="https://www.linkedin.com/in/ethan-nichols-8a3bb3188/">LinkedIn</a></button></li>
+        <li><button><a href="https://ethan-nichols.com/hero">My Website</a></button></li>
+        <li><button><a href="https://mydjangobugtracker.herokuapp.com/">Django Bug Tracker</a></button></li>
+        <li><button><a href = "mailto: eanichols97@gmail.com">Send Email</a></button></li>
+
+        
+        
+        
+        
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
